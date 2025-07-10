ad3036e9f4f23d2fb53a0794cbbeffb06f997b63
Ridwan Oladipupo <ridwan@spektra.co>
2025-07-10 10:53:53 -0300

    added lazy loading, protected route, swagger doc

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(
     },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    // credentials: true,
   })
 );
 
