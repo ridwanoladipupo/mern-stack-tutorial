2a19bd472b021f2287fbfb35b6a7640a8a97db49
Ridwan Oladipupo <ridwan@spektra.co>
2025-07-10 01:38:25 -0300

    update header, cors

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -45,7 +45,9 @@ export const login = async (req, res) => {
     if (!isMatch)
       return res.status(400).json({ message: "Invalid credentials" });
 
-    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1h" });
+    const token = jwt.sign({ id: user._id, role: user.role }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
 
     res.status(200).json({
       user: { id: user._id, name: user.name, email: user.email },
